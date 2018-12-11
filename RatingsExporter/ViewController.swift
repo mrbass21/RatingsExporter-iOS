be609ff20df23d85579af8e25dd993aac725e656
Jason Beck <mrbass21@gmail.com>
2018-12-10 23:18:53 -0600

    Initial Commit

---
+++ RatingsExporter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RatingsExporter
+//
+//  Created by Jason Beck on 12/10/18.
+//  Copyright © 2018 Jason Beck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
