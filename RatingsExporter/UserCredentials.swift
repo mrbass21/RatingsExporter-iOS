0025cadf6350de9b5a5c21f63fdc087c5e7d3d21
Jason Beck <mrbass21@gmail.com>
2018-12-13 13:12:21 -0600

    Got rid of some properties in favor of just using computed properties. Added Comments. Refactored get to store UTF-8 data explicitly instead of just a "String"

--- RatingsExporter/UserCredentials.swift
+++ RatingsExporter/UserCredentials.swift
@@ -9,6 +9,14 @@
 import Foundation
 import Security
 
+// The original goal of this struct is to privide a single place to get the credentials as easily as possible
+// from the callers perspctive. However, currently this design does not allow this struct to throw on error with
+// keychain errors, so it's up to the caller to check that if they try and set a cookie, then try to get it again
+// and it's nil, they handle the problem.
+//
+// A possible redesign of this is to make the computed properties functions that can throw to report keychain errors
+// up one more layer to the consumer and let them handle the errors.
+
 struct UserCredentials {
     
     private struct Keychain {
@@ -18,7 +26,7 @@ struct UserCredentials {
             case unexpectedError(status: OSStatus)
         }
         
-        struct KeychainIDs {
+         struct KeychainIDs {
             static let netflixID = "NetflixID"
             static let netflixSecretID = "NetflixSecretID"
         }
@@ -41,21 +49,10 @@ struct UserCredentials {
         }
     }
     
-    //MARK: - Internal Storage
-    private lazy var _netflixID: String? = {
-        //Lookup in Keychain
-        return getCookie(name: Keychain.KeychainIDs.netflixID)
-    }()
-
-    private lazy var _netflixSecureID: String? = {
-       return getCookie(name: Keychain.KeychainIDs.netflixSecretID)
-    }()
-    
-    
     //MARK: - External Access
     public var netflixID: String? {
-        mutating get{
-            return _netflixID
+        get {
+            return getCookie(name: Keychain.KeychainIDs.netflixID)
         }
         
         set {
@@ -64,17 +61,32 @@ struct UserCredentials {
     }
     
     public var netflixSecureID: String? {
-        mutating get {
-            return _netflixSecureID
+        get {
+            return getCookie(name: Keychain.KeychainIDs.netflixSecretID)
         }
         
         set {
-            
+            storeCookie(name: Keychain.KeychainIDs.netflixSecretID, value: newValue)
         }
     }
     
     //MARK: - Private Methods
     private func storeCookie(name: String, value: String?) {
+        
+        //Figure out if we need to create or update the keychain item.
+        do {
+             let _ = try getCookieKeychainItem(name: name)
+            //It didn't throw here, so that means that we already have a keychain value for this. Update it.
+        } catch Keychain.KeychainError.notFound {
+            //Create the keychain item
+        } catch Keychain.KeychainError.unexpectedError(status: let status)
+        {
+            //Trying to get the keychain item failed horribly.
+            print("Unexpected error from keychain get inside storeCookie! Keychain OSStaus error: \(status)")
+        } catch {
+            //Other failures...
+            print("Keychain error retrieving \(name)")
+        }
         
         let keychainDescription = "Stores the netflix cookie \(name) in keychain to fetch ratings for this user"
         
@@ -88,6 +100,7 @@ struct UserCredentials {
             print("Unexpected keychain error: \(String(describing: SecCopyErrorMessageString(status, nil)))")
             return nil
         } catch {
+            print("\(name) not found in keychain")
             return nil
         }
     }
@@ -105,6 +118,7 @@ struct UserCredentials {
         var keychainQuery = Keychain.queryDictionary
         keychainQuery[kSecAttrAccount] = name as CFString
         
+        //Query and set the object from keychain
         var returnQueryCookie: AnyObject?
         let status = withUnsafeMutablePointer(to: &returnQueryCookie) {
             SecItemCopyMatching(keychainQuery as CFDictionary, UnsafeMutablePointer($0))
@@ -115,15 +129,19 @@ struct UserCredentials {
             throw Keychain.KeychainError.notFound
         }
         
+        //If there's any error other than the one above, panic!
         guard status == noErr else {
             throw Keychain.KeychainError.unexpectedError(status: status)
         }
         
-        guard let returnCookie = returnQueryCookie as? [CFString: AnyObject], let returnCookieValue = returnCookie[kSecValueData] as? String else {
+        //Do a bunch of binding to check everything goes okay
+        guard let returnCookie = returnQueryCookie as? [CFString: AnyObject],
+                let returnCookieValue = returnCookie[kSecValueData] as? Data,
+                let returnCookieString = String(bytes: returnCookieValue, encoding: String.Encoding.utf8) else {
             throw Keychain.KeychainError.badData
         }
         
-        return returnCookieValue
+        return returnCookieString
     }
     
     private func deleteCookieKeychainItem() {
