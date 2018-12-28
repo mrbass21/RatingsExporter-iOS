45e177fb3e138a904319fdd1ae6d0b52a1cd60d0
Jason Beck <mrbass21@gmail.com>
2018-12-28 17:19:06 -0600

    Made suggestions for slight restructure from Doug Cobb. Added unit test with invalid server info, but good certificate.

--- RatingsExporterTests/NetflixLoginViewControllerTests.swift
+++ RatingsExporterTests/NetflixLoginViewControllerTests.swift
@@ -7,13 +7,14 @@
 //
 
 import XCTest
+import WebKit
 @testable import RatingsExporter
 
 
 class NetflixLoginViewControllerTests: XCTestCase {
     
-    var controllerUnderTest: NetflixLoginViewController!
-    var bundle: Bundle?
+     var controllerUnderTest: NetflixLoginViewController = (UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: NetflixLoginViewController.Identifiers.Storyboard.NetflixLoginController) as! NetflixLoginViewController)
+    var bundle: Bundle = Bundle.init(for: NetflixLoginViewController.classForCoder())
     
     enum TestCertType: String {
         case goodNetflixCert = "netflix"
@@ -21,13 +22,11 @@ class NetflixLoginViewControllerTests: XCTestCase {
     }
 
     override func setUp() {
-        bundle = Bundle.init(for: type(of: self))
-        controllerUnderTest = (UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: NetflixLoginViewController.Identifiers.Storyboard.NetflixLoginController) as! NetflixLoginViewController)
-        let _ = controllerUnderTest.view
+        super.setUp()
     }
 
     override func tearDown() {
-        controllerUnderTest = nil
+        super.tearDown()
     }
 
     func testValidNetflixCertPinning() {
@@ -54,13 +53,45 @@ class NetflixLoginViewControllerTests: XCTestCase {
         let authenticationChallenge = URLAuthenticationChallenge(protectionSpace: mockURLProtection, proposedCredential: nil, previousFailureCount: 0, failureResponse: nil, error: nil, sender: self)
     
         //then
-        controllerUnderTest.webView(controllerUnderTest.loginWebView, didReceive: authenticationChallenge) { (disposition, credential) in
+        let webView = WKWebView(frame: CGRect.zero)
+        controllerUnderTest.webView(webView, didReceive: authenticationChallenge) { (disposition, credential) in
             XCTAssertEqual(disposition, .useCredential)
+        }
+    }
+    
+    func testInvalidNetflixServerInfo() {
+        //given
+        
+        //Create items to make the SecTrust
+        let goodCertificate = getCertificateForTest(type: .goodNetflixCert)!
+        let policy = SecPolicyCreateSSL(true, "www.netflax.com" as CFString)
+        
+        //Create the SecTrust
+        var serverTrust: SecTrust?
+        let status = withUnsafeMutablePointer(to: &serverTrust) { (serverTrust) -> OSStatus in
+            return SecTrustCreateWithCertificates(goodCertificate, policy, serverTrust)
+        }
+        
+        //Fail if error was encountered
+        guard status == noErr else {
+            XCTFail("Failed to create ServerTrust object")
+            return
+        }
+        
+        //Create the mock authentication challenge
+        let mockURLProtection = MockURLProtectionSpace(host: "www.netflax.com", port: 443, protocol: "https", realm: nil, authenticationMethod: NSURLAuthenticationMethodServerTrust, serverTrust: serverTrust!)
+        let authenticationChallenge = URLAuthenticationChallenge(protectionSpace: mockURLProtection, proposedCredential: nil, previousFailureCount: 0, failureResponse: nil, error: nil, sender: self)
+        
+        //then
+        let webView = WKWebView(frame: CGRect.zero)
+        controllerUnderTest.webView(webView, didReceive: authenticationChallenge) { (disposition, credential) in
+            XCTAssertEqual(disposition, URLSession.AuthChallengeDisposition.cancelAuthenticationChallenge)
         }
     }
 
 }
 
+//MARK: - URLAuthenticationChallengeSender
 extension NetflixLoginViewControllerTests: URLAuthenticationChallengeSender {
     func use(_ credential: URLCredential, for challenge: URLAuthenticationChallenge) {
     }
@@ -71,10 +102,11 @@ extension NetflixLoginViewControllerTests: URLAuthenticationChallengeSender {
     func cancel(_ challenge: URLAuthenticationChallenge) {
     }
 }
+
 //MARK: - Helper Methods
 extension NetflixLoginViewControllerTests {
     func getCertificateForTest(type identifier: TestCertType) -> SecCertificate? {
-        let certURL = bundle!.url(forResource: identifier.rawValue, withExtension: "cer")!
+        let certURL = bundle.url(forResource: identifier.rawValue, withExtension: "cer")!
         let certData = try! Data(contentsOf: certURL)
         
         return SecCertificateCreateWithData(nil, certData as CFData)
