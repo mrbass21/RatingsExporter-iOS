30209e0086d91f1389014557f305c38a3df9a88a
Jason Beck <mrbass21@gmail.com>
2019-01-21 16:45:17 -0600

    Finished some missed refactoring of UserCredential, removed unneeded imports.

--- RatingsExporter/Models/Credentials/UserCredential.swift
+++ RatingsExporter/Models/Credentials/UserCredential.swift
@@ -6,14 +6,10 @@
 //  Copyright © 2018 Jason Beck. All rights reserved.
 //
 
-import Foundation
-import Security
-import WebKit
-
 //Currently there are no common credential items to conform to, but this architechture is based off the concept
 //of a base credential.
 
-//MARK: - UserCredentialsProtocol
+//MARK: - UserCredentialProtocol
 ///A protocol that defines a credential used for some service.
 public protocol UserCredentialProtocol: Equatable {
 }
